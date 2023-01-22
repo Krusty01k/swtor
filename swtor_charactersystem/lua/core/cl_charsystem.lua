8004255e7b63198c3d3e83fce085901bea6cf206
Maximilian <101973951+Krusty01k@users.noreply.github.com>
2023-01-22 23:41:07 +0100

    Faction Select
    Added some Faction Select stuff, nothing important.

--- swtor_charactersystem/lua/core/cl_charsystem.lua
+++ swtor_charactersystem/lua/core/cl_charsystem.lua
@@ -1,1 +1,40 @@
--- ;)
+SCS = SCS or {}
+
+local background = Material("background.png")
+
+local factions = {
+    "Sith" = {
+        name = "Sith Imperium",
+        logo = Material("sith_logo.png"),
+    },
+    "Republic" = {
+        name = "Galactic Republic",
+        logo = Material("jedi_logo.png"),
+    },
+}
+
+local function SCS.FactionSelectMenu()
+    local SCS.FactionSelectBackground = vgui.Create("DFrame")
+    SCS.FactionSelectBackground:SetSize(surface.ScreenWidth(), surface.ScreenHeight())
+    SCS.FactionSelectBackground:SetPos(0, 0)
+    SCS.FactionSelectBackground:SetTitle("")
+    SCS.FactionSelectBackground:SetDraggable(false)
+    SCS.FactionSelectBackground:ShowCloseButton(false)
+    SCS.FactionSelectBackground:MakePopup()
+    SCS.FactionSelectBackground.Paint = function(self, w, h)
+        surface.SetDrawColor(255, 255, 255, 255)
+        surface.SetMaterial(background)
+        surface.DrawTexturedRect(0, 0, w, h)
+    end
+    
+    local SCS.FactionSelectButton = SCS.FactionSelectBackground:Add("DButton")
+    SCS.FactionSelectButton:SetSize(200, 50)
+    SCS.FactionSelectButton:SetPos(0, 0)
+    SCS.FactionSelectButton:SetText(" ")
+    SCS.FactionSelectButton.Paint = function(self, w, h)
+        surface.SetDrawColor(255, 255, 255, 255)
+        surface.SetMaterial(factions.logo)
+        surface.DrawTexturedRect(0, 0, w, h)
+    end
+
+end 
--- swtor_charactersystem/lua/core/sv_charsystem.lua
+++ swtor_charactersystem/lua/core/sv_charsystem.lua
@@ -1,1 +1,6 @@
--- :)
+util.AddNetworkString("SWTOR.OpenCharMenuOnSpawn")
+
+hook.Add("PlayerInitialSpawn", "SWTOR.OpenCharMenuOnSpawn", function(ply)
+    net.Start("SWTOR.OpenCharMenuOnSpawn")
+    net.Send(ply)
+end) 
