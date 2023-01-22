65d6bf831e8792acafcde4ee704dc5f9aa498e5e
Maximilian <101973951+Krusty01k@users.noreply.github.com>
2023-01-22 22:38:05 +0100

    Add files via upload

---
+++ swtor_charactersystem/lua/autorun/sh_index.lua
@@ -0,0 +1,18 @@
+SWTOR = SWTOR or {}
+SWTOR.CHARACTER = SWTOR.CHARACTER or {}
+
+-- For Server files
+if SERVER then 
+    AddCSLuaFile("core/cl_charsystem.lua")
+    AddCSLuaFile("sh_charsys_config.lua")
+
+    include("core/sv_charsystem.lua")
+end
+
+-- For Client files
+if CLIENT then
+    include("core/cl_charsystem.lua")
+end
+
+-- For Shared files 
+include("sh_charsys_config.lua")
---
+++ swtor_charactersystem/lua/sh_charsys_config.lua
@@ -0,0 +1,14 @@
+SWTOR.CHARACTER.Config = {}
+
+SWTOR.CHARACTER.Config.MaxCharacter = 5 -- Max Character
+SWTOR.CHARACTER.Config.Factions = {
+    ["Imperium"] = {
+        TEAM_AKOLYTH,
+    },
+    ["Republik"] = {
+        TEAM_STUDENT,
+    },
+    ["Neutral"] = {
+        TEAM_CIVILIAN,  
+    },
+}
